370c59c68f8c1a49a990f70f9c4763399abbce89
Nidhi Bhamare <119914367+Nidhi-bhamare@users.noreply.github.com>
2025-06-22 10:58:49 +0530

    Time: 2 ms (18.81%), Space: 9.6 MB (99.67%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
@@ -7,12 +7,12 @@ public:
         for (int i = 0; i < n; i += k) {
             string temp = "";
 
-            // k characters भरतो, जर शेवटचे कमी असतील तर fill करतो
+           
             for (int j = i; j < i + k && j < n; j++) {
                 temp += s[j];
             }
 
-            // जर temp ची लांबी कमी असेल, तर fill letter टाकतो
+         
             while (temp.length() < k) {
                 temp += fill;
             }
